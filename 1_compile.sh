e814efb3ed12936bc62f0fdbbb099fd12764133e
sehraf <sehraf42@gmail.com>
2022-04-11 18:10:51 +0200

    shellcheck

--- 1_compile.sh
+++ 1_compile.sh
@@ -3,24 +3,20 @@
 set -e
 # set -x
 
-source ./consts.sh
+. ./consts.sh
 
-function clean_dir() {
+clean_dir() {
     _DIR=${1}
 
     # kind of dangerous ...
-    [ "${_DIR}" == '/' ] && extit -1
-    rm -rf ${_DIR} || true
+    [ "${_DIR}" = '/' ] && extit 1
+    rm -rf "${_DIR}" || true
 }
 
-function pin_commit() {
+pin_commit() {
     _COMMIT=${1}
-
-    pushd ${2}
-    _COMMIT_IS=`git rev-parse HEAD`
-    popd
-
-    [ "${IGNORE_COMMITS}" != '0' ] || [ ${_COMMIT} == ${_COMMIT_IS} ] || exit -1
+    _COMMIT_IS=$(git rev-parse HEAD)
+    [ "${IGNORE_COMMITS}" != '0' ] || [ "${_COMMIT}" = "${_COMMIT_IS}" ] || exit 1
 }
 
 for DEP in riscv64-linux-gnu-gcc swig ; do
@@ -28,7 +24,7 @@ for DEP in riscv64-linux-gnu-gcc swig ; do
 done
 
 mkdir -p build
-mkdir -p ${OUT_DIR}
+mkdir -p "${OUT_DIR}"
 cd build
 
 if [ ! -f "${OUT_DIR}/boot0_sdcard_sun20iw1p1.bin" ] ; then
@@ -37,11 +33,11 @@ if [ ! -f "${OUT_DIR}/boot0_sdcard_sun20iw1p1.bin" ] ; then
     clean_dir ${DIR}
 
     git clone https://github.com/smaeul/sun20i_d1_spl
-    cd ${DIR}
-    git checkout ${COMMIT_BOOT0}
-    make CROSS_COMPILE=${CROSS_COMPILE} p=sun20iw1p1 mmc
+    cd "${DIR}"
+    git checkout "${COMMIT_BOOT0}"
+    make CROSS_COMPILE="${CROSS_COMPILE}" p=sun20iw1p1 mmc
     cd ..
-    cp ${DIR}/nboot/boot0_sdcard_sun20iw1p1.bin ${OUT_DIR}
+    cp ${DIR}/nboot/boot0_sdcard_sun20iw1p1.bin "${OUT_DIR}"
 fi
 
 if [ ! -f "${OUT_DIR}/u-boot.toc1" ] ; then
@@ -52,7 +48,7 @@ if [ ! -f "${OUT_DIR}/u-boot.toc1" ] ; then
     git clone https://github.com/smaeul/opensbi
     cd ${DIR}
     git checkout d1-wip
-    make CROSS_COMPILE=${CROSS_COMPILE} PLATFORM=generic FW_PIC=y FW_OPTIONS=0x2
+    make CROSS_COMPILE="${CROSS_COMPILE}" PLATFORM=generic FW_PIC=y FW_OPTIONS=0x2
     cd ..
     # cp opensbi/build/platform/generic/firmware/fw_dynamic.bin ${OUT_DIR}
 
@@ -63,11 +59,11 @@ if [ ! -f "${OUT_DIR}/u-boot.toc1" ] ; then
     git clone https://github.com/smaeul/u-boot.git
     cd ${DIR}
     git checkout d1-wip
-    pin_commit ${COMMIT_UBOOT} .
+    pin_commit "${COMMIT_UBOOT}"
 
-    make CROSS_COMPILE=${CROSS_COMPILE} ARCH=${ARCH} nezha_defconfig
+    make CROSS_COMPILE="${CROSS_COMPILE}" ARCH="${ARCH}" nezha_defconfig
     # make CROSS_COMPILE=${CROSS_COMPILE} ARCH=${ARCH} lichee_rv_defconfig
-    make CROSS_COMPILE=${CROSS_COMPILE} ARCH=${ARCH} -j `nproc`
+    make CROSS_COMPILE="${CROSS_COMPILE}" ARCH="${ARCH}" -j "${NPROC}"
     cd ..
     # cp u-boot/arch/riscv/dts/sun20i-d1-lichee-rv-dock.dtb ${OUT_DIR}
 
@@ -84,7 +80,7 @@ file = u-boot/u-boot-nodtb.bin
 addr = 0x4a000000
 EOF
     ${DIR}/tools/mkimage -T sunxi_toc1 -d licheerv_toc1.cfg u-boot.toc1
-    cp u-boot.toc1 ${OUT_DIR}
+    cp u-boot.toc1 "${OUT_DIR}"
 fi
 
 if [ ! -f "${OUT_DIR}/boot.scr" ] ; then
@@ -98,9 +94,9 @@ load mmc 0:1 ${kernel_addr_r} Image
 echo "Booting kernel with bootargs as $bootargs; and fdtcontroladdr is $fdtcontroladdr"
 booti ${kernel_addr_r} - ${fdtcontroladdr}
 EOF
-    ${DIR}/tools/mkimage -T script -C none -O linux -A ${ARCH} -d bootscr.txt boot.scr
+    ${DIR}/tools/mkimage -T script -C none -O linux -A "${ARCH}" -d bootscr.txt boot.scr
     rm bootscr.txt
-    cp boot.scr ${OUT_DIR}
+    cp boot.scr "${OUT_DIR}"
 fi
 
 if [ ! -f "${OUT_DIR}/Image" ] || [ ! -f "${OUT_DIR}/Image.gz" ] ; then
@@ -113,7 +109,9 @@ if [ ! -f "${OUT_DIR}/Image" ] || [ ! -f "${OUT_DIR}/Image.gz" ] ; then
 
     # try not to clone complete linux source tree here!
     git clone --depth 1 https://github.com/smaeul/linux -b riscv/d1-wip
-    pin_commit ${COMMIT_KERNEL} ${DIR}
+    cd ${DIR}
+    pin_commit "${COMMIT_KERNEL}"
+    cd ..
 
     # LicheeRV defconfig
     # mkdir -p linux-build/arch/riscv/configs
@@ -125,7 +123,7 @@ if [ ! -f "${OUT_DIR}/Image" ] || [ ! -f "${OUT_DIR}/Image.gz" ] ; then
     echo 'CONFIG_WIRELESS=y' >> ${DIR}/arch/riscv/configs/nezha_defconfig
     echo 'CONFIG_CFG80211=m' >> ${DIR}/arch/riscv/configs/nezha_defconfig
 
-    make ARCH=${ARCH} -C linux O=../linux-build nezha_defconfig
+    make ARCH="${ARCH}" -C linux O=../linux-build nezha_defconfig
 
     # Archlinux PR #1001 https://github.com/felixonmars/archriscv-packages/pull/1001 config
     # https://github.com/felixonmars/archriscv-packages/blob/6689a4fdcd76dbbab777803493873f65e127e3e6/linux-nezha-git/config
@@ -133,9 +131,9 @@ if [ ! -f "${OUT_DIR}/Image" ] || [ ! -f "${OUT_DIR}/Image.gz" ] ; then
     # cp ../arch_nezha_git_config linux-build/arch/riscv/configs/licheerv_defconfig
     # make ARCH=${ARCH} -C linux O=../linux-build licheerv_defconfig
 
-    make CROSS_COMPILE=${CROSS_COMPILE} ARCH=${ARCH} -j `nproc` -C linux-build
-    cp linux-build/arch/riscv/boot/Image.gz ${OUT_DIR}
-    cp linux-build/arch/riscv/boot/Image ${OUT_DIR}
+    make CROSS_COMPILE="${CROSS_COMPILE}" ARCH="${ARCH}" -j "${NPROC}" -C linux-build
+    cp linux-build/arch/riscv/boot/Image.gz "${OUT_DIR}"
+    cp linux-build/arch/riscv/boot/Image "${OUT_DIR}"
 fi
 
 if [ ! -f "${OUT_DIR}/8723ds.ko" ] ; then
@@ -145,7 +143,7 @@ if [ ! -f "${OUT_DIR}/8723ds.ko" ] ; then
 
     git clone https://github.com/lwfinger/rtl8723ds.git
     cd ${DIR}
-    make CROSS_COMPILE=${CROSS_COMPILE} ARCH=${ARCH} KSRC=../linux-build -j `nproc`  modules || true
+    make CROSS_COMPILE="${CROSS_COMPILE}" ARCH="${ARCH}" KSRC=../linux-build -j "${NPROC}"  modules || true
     cd ..
-    cp ${DIR}/8723ds.ko ${OUT_DIR}
+    cp ${DIR}/8723ds.ko "${OUT_DIR}"
 fi
