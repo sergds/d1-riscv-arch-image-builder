33026e7f8c753fad46a62cce0a97ddb7cfedce45
sehraf <sehraf42@gmail.com>
2022-07-20 17:44:45 +0200

    update

--- consts.sh
+++ consts.sh
@@ -23,10 +23,16 @@ export SUDO='sudo'
 # use arch-chroot?
 export USE_CHROOT=1
 
-# use extlinux ('extlinux') or boot.scr ('script')?
+# use extlinux ('extlinux') or boot.scr ('script') for loading the kernel?
 export BOOT_METHOD='extlinux'
 
 # pinned commits (no notice when things change)
+export SOURCE_BOOT0='https://github.com/smaeul/sun20i_d1_spl'
+export SOURCE_OPENSBI='https://github.com/smaeul/opensbi'
+export SOURCE_UBOOT='https://github.com/smaeul/u-boot'
+export SOURCE_KERNEL='https://github.com/smaeul/linux'
+# https://github.com/karabek/xradio
+
 export COMMIT_BOOT0='882671fcf53137aaafc3a94fa32e682cb7b921f1' # from 14.06.2022
 export COMMIT_UBOOT='afc07cec423f17ebb4448a19435292ddacf19c9b' # equals d1-wip (28.05.2022)
 export COMMIT_KERNEL='fe178cf0153d98b71cb01a46c8cc050826a17e77' # equals riscv/d1-wip head
