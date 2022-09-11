184d49e700ea34ec134698d6bfb2a00f5123e04f
Etzyio <36181835+Etzyio@users.noreply.github.com>
2022-09-11 16:13:41 +0800

    fix: Without Choose 'build img continue'

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ sudo losetup /dev/loop17 ./archlinux_riscv.img
 docker run --name build_riscv riscv-arch-image:${1}
 docker cp build_riscv:/home/runner/work/riscv-arch-image-builder/riscv-arch-image-builder ../
 sed -i 's/USE_CHROOT=1/USE_CHROOT=0/g' consts.sh
+sed -i 's/read -r  confirm/#read -r  confirm/g' 2_create_sd.sh
 sudo ./2_create_sd.sh /dev/loop17
 tar -zcvf archlinux_riscv.img.tar.gz archlinux_riscv.img
