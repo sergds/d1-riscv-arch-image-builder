ffac80631a0562d6114d4b4871b94e7552d55968
sehraf <sehraf42@gmail.com>
2023-07-25 19:23:16 +0200

    debug pipeline ... we are getting closer

--- build_img.sh
+++ build_img.sh
@@ -4,7 +4,8 @@ sudo losetup /dev/loop17 ./archlinux_riscv.img
 docker run --name build_riscv riscv-arch-image:${1}
 docker cp build_riscv:/home/runner/work/riscv-arch-image-builder/riscv-arch-image-builder .
 pwd
-ls ../riscv-arch-image-builder
-ls ../riscv-arch-image-builder/output
+ls 
+ls riscv-arch-image-builder
+ls riscv-arch-image-builder/output
 sudo CI_BUILD=1 ./2_create_sd.sh /dev/loop17
 tar -zcvf archlinux_riscv.img.tar.gz archlinux_riscv.img
